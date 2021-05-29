ffdab06a7d3ba8933cf5456cef6a4d65861d72a9
ict283 <glennlum@icloud.com>
2021-05-29 13:07:12 +0800

    Implement Prime class

--- unitTest/Prime.cpp
+++ unitTest/Prime.cpp
@@ -1,3 +1,29 @@
 // For review purposes only! Does not represent the actual solution!
 
 #include "Prime.h"
+
+Prime::Prime(){
+    //empty
+}
+
+Prime::~Prime(){
+    //empty
+}
+
+bool Prime::isPrime(const int n){
+    
+    //Got this off the internet, don't know if it works
+    if(isdigit(n)!=0){
+        return false;
+    }
+    if(n==0||n==1){
+        return false;
+    }else{
+        for(int i =2; i<= n/2; ++i){
+            if(n%i==0){
+                return false;
+            }
+        }
+    }
+    return true;
+}
