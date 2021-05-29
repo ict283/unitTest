8238d4b37a597b6130c4652c772ba55ab9891372
ict283 <glennlum@icloud.com>
2021-05-29 13:16:40 +0800

    Refactor isPrime - testNegativeNumIsFalse failed

--- unitTest/Prime.cpp
+++ unitTest/Prime.cpp
@@ -16,7 +16,7 @@ bool Prime::isPrime(const int n){
     if(isdigit(n)!=0){
         return false;
     }
-    if(n==0||n==1){
+    if(n==0||n==1||n<0){
         return false;
     }else{
         for(int i =2; i<= n/2; ++i){
